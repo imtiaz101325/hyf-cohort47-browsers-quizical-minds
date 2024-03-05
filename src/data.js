823b3e26782ba4e5f4479b37bf4a0f3d90a136ea
Lidya Tesfamariam <zenilisa1221@gmail.com>
2024-03-05 03:01:51 +0300

    finished task from phase 1

--- src/data.js
+++ src/data.js
@@ -56,5 +56,173 @@ export const quizData = {
       ],
     },
     // Add more questions here
+    {
+      text: '	Which of the following laws is associated with the heating effect of electric current ?',
+      answers: {
+        a: "Joule's law",
+        b: "Ohm's law ",
+        c: "Faraday's law ",
+        d: "Newton's law ",
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Who demonstrated that objects of different masses would reach the ground together when dropped from the same height?',
+      answers: {
+        a: "Isaac Newton",
+        b: "Archimedes ",
+        c: "Robert Boyle ",
+        d: "Galileo Galilei ",
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: '	A body at rest can have ?',
+      answers: {
+        a: "speed",
+        b: "velocity",
+        c: "momentum",
+        d: "Energy",
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: '	The pH of pure water is ?',
+      answers: {
+        a: '2',
+        b: '6',
+        c: '7',
+        d: '8',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'What is the ability of a metal to be drawn into thin wires called ?',
+      answers: {
+        a: 'Ductility',
+        b: 'Sonorous ',
+        c: 'Malleability',
+        d: 'Intractability',
+      },
+      correct: 'd',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'The sex of the children will be determined by what they inherit from the ?',
+      answers: {
+        a: 'nature',
+        b: 'father',
+        c: 'mother',
+        d: 'mother and father'
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'If the mass of a solid is doubled, its density ?',
+      answers: {
+        a: 'becomes two times',
+        b: 'becomes half ',
+        c: 'remains unchanged',
+        d: 'becomes four times',
+      },
+      correct: 'b',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
+    {
+      text: 'Fats and oils become rancid because of ?',
+      answers: {
+        a: 'transpiration ',
+        b: 'reduction ',
+        c: 'oxidation',
+        d: 'corrosion '
+      },
+      correct: 'a',
+      selected: null,
+      links: [
+        {
+          text: 'javascript.info',
+          href: 'https://javascript.info/variables',
+        },
+        {
+          text: 'Tyler McGinnis',
+          href: 'https://ui.dev/var-let-const/',
+        },
+      ],
+    },
   ],
 };
