84668caeafb96cfb5783e995a89e03d49e64f79a
Ben Gumaan <bot.s8x@gmail.com>
2024-03-09 20:47:32 +0100

    Fixed some issues with the design

--- src/pages/finalPage.js
+++ src/pages/finalPage.js
@@ -1,5 +1,8 @@
 import { createFinalElement } from '../views/finalPageView.js';
 import { USER_INTERFACE_ID } from '../constants.js';
+import { quizData } from '../data.js';
+import { initWelcomePage } from '../pages/welcomePage.js';
+import { createRestartButton } from '../views/finalPageView.js';
 
 export const initFinalPage = (counter, questions) => {
   const userInterface = document.getElementById(USER_INTERFACE_ID);
@@ -7,4 +10,19 @@ export const initFinalPage = (counter, questions) => {
 
   const finalPage = createFinalElement(counter, questions);
   userInterface.appendChild(finalPage);
+
+  // Restart button
+  const restartButton = createRestartButton();
+  restartButton.addEventListener('click', restartQuiz);
+  userInterface.appendChild(restartButton);
+};
+
+export const restartQuiz = () => {
+  // Reset the quiz data and start a new quiz
+  quizData.currentQuestionIndex = 0;
+  quizData.score = 0;
+  quizData.questionsTracker = [];
+  localStorage.clear();
+
+  initWelcomePage();
 };
